64da2a96db7feaaa94053fa436a3319c54d0d1f8
IZ0FIU <65508773+IZ0FIU@users.noreply.github.com>
2020-06-10 12:00:03 +0200

    Update Dxspotdisplay.py

--- Dxspotdisplay.py
+++ Dxspotdisplay.py
@@ -15,7 +15,7 @@ lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1,
               auto_linebreaks=True,
               backlight_enabled=True)
 
-tn = telnetlib.Telnet('node address', port) #please enter address and port node
+tn = telnetlib.Telnet('node address', port) #please enter DX Cluster address and port node
 tn.read_until(b"login: ")
 tn.write('your callsign' + "\n") #please enter your callsign
 time.sleep(5)
