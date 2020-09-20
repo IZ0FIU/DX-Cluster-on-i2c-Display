ebcd1d34576922863cbbe3cb4a41fd6cfe557687
IZ0FIU <65508773+IZ0FIU@users.noreply.github.com>
2020-09-20 12:09:13 +0200

    Update Dxspotdisplay.py

--- Dxspotdisplay.py
+++ Dxspotdisplay.py
@@ -7,7 +7,6 @@ import time
 import getpass
 import sys
 import telnetlib
-from RPLCD.i2c import CharLCD
 
 lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1,
               cols=20, rows=4, dotsize=8,
